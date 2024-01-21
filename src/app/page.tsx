fc1dc066b78cd068b36a1e3f249f0e6830a15530
quochung <maphquochung@gmail.com>
2024-01-21 18:43:21 +0700

    aaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,14 +24,14 @@ export default function HomePage() {
   
 
   return (
-    <section className="mb-16 mt-0 space-y-8 md:mt-20">
+    <section className="mb-16 mt-0 space-y-8 md:mt-*">
         <h1 className="mt-12 text-center text-3xl font-bold">Test API</h1>
-        <form onSubmit={handleChange}>
+        <form onSubmit={handleChange} className="mb-16 mt-0 space-y-3 md:mt-20">
         <input 
           type="text" 
           id="first_name" 
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-          placeholder="Input: Dịch vụ tệ quá! | Trường rất đẹp | Khi nào có đợt tuyển sinh mới vậy? ...." 
+          placeholder='"Input: Dịch vụ tệ quá!" | "Trường rất đẹp" | "Khi nào có đợt tuyển sinh mới vậy?" | ....'
           value={text}
           onChange={(e) => setText(e.target.value)}
           required 
