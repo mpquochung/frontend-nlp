206aca8cb9df451d53a7d694e9068a9e41aaebc9
quochung <maphquochung@gmail.com>
2024-02-09 18:23:42 +0700

    update author page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,8 +2,7 @@
 import React from 'react';
 import Paragraph from '@/components/Paragraph';
 import Title from '@/components/BigTitle';
-export default function AboutPage() {
-                
+export default function AboutPage() {         
   const title0 = {title:'About this Project',
                    align:'center'};
 
@@ -11,7 +10,7 @@ export default function AboutPage() {
                   data : ["This website is the implementation of our research conducted at NEU-xyz. Utilizing a multi-task model, we offer the capability to provide multiple predictions simultaneously. Our model specializes in identifying current trends and analyzing user behavior specifically within education forums. You can experiment with our prediction feature here or explore our dashboard to delve deeper into forum analysis here."],
                   align: "start"};
 
-  const title1 = {title: 'What We Have',
+  const title1 = {title: 'What We Bring',
                   align: "center"}
               
   const text2 = {title: "Multi-task BERT Model",
@@ -31,7 +30,7 @@ export default function AboutPage() {
 
   const text5 = {title: "It is not just a normal forum !",
                   data: ["Regular forums serve as platforms for general discussions, often revolving around various topics of interest, entertainment, or community engagement. On the other hand, educational forums are specifically tailored to facilitate learning, personal life sharing among students, educators, and enthusiasts. Educational forums typically focus on academic subjects, professional development, or skill enhancement, providing a structured environment for asking questions, exchanging ideas, sharing students’ lifes and accessing educational resources. "],
-                  align: "center",
+                  
                   };
           
   const title3 = {title: 'Existing Solution vs Our Approaches',
@@ -40,17 +39,6 @@ export default function AboutPage() {
   const text6 = {title :"More precise and x2 efficiency!",
                   data: ["There have been numerous approaches for this problem, from datasets specialized for education forums, e,g, (VSFC) to models. However, those datasets are too naive, as they don’t contain any slang among ‘Gen-Z’ students, or have little relevance to education, and the models are unnecessarily sophisticated."," By only using one BERT-base model, we are able to perform both tasks in one single run, faster than any existing BERTology that is close to our performance. And with an up-to-date dataset, we can better extract information on education on social media platform, namely Facebook."],
                   align: "start"};
-
-  const title4 = {title: 'Who are we?',
-                  align: "center"}
-                
-  const text7 ={ title:'',
-                  data: ["Our team consists of four talented undergraduate students from National Economic University, sharing the same passion for NLP and machine learning. We are dedicated to pushing the boundaries of what's possible with AI and delivering innovative solutions that meet the needs of our users."],
-                  align: "center"};
-
-  const text8 = {title: "Get in Touch",
-                  data:["We're always excited to hear from our users and welcome any feedback or inquiries. Whether you have questions about our platform, suggestions for improvement, or are interested in collaboration opportunities, please don't hesitate to reach out to us. ","Thank you for spending your time on our platform."]
-                  ,align: "start"};
 
   return (
 <>
@@ -64,10 +52,6 @@ export default function AboutPage() {
     <Paragraph data={text5}/>
     <Title data={title3}/>
     <Paragraph data={text6}/>
-    <Title data={title4}/>
-    <Paragraph data={text7}/>
-    <Paragraph data={text8}/>
-
 </>             
   );
 }
---
+++ src/app/contact/author.tsx
@@ -0,0 +1,39 @@
+export const giang = {
+    name: 'Giuong Phuong Giang',
+    info: 'Student at NEU',
+    position: 'Data Anlyst & Visualization',
+    url: 'https://natural-rhubarb-e01.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F4aa398d4-2e92-48b7-92f9-4d87362fab14%2F9e9f8e9c-2c93-4c24-8af1-85cac9975483%2FIMG_8802.jpg?table=block&id=ebd8bc46-6d07-4b09-9646-7653f3553fba&spaceId=4aa398d4-2e92-48b7-92f9-4d87362fab14&width=2000&userId=&cache=v2', 
+  }
+  
+export  const quoc = {
+    name: 'Mai Phan Quoc Hung',
+    info: 'Student at NEU',
+    position: 'Model Assistant & Frontend Developer',
+    url: 'https://natural-rhubarb-e01.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F4aa398d4-2e92-48b7-92f9-4d87362fab14%2F31a8467e-a6a2-499c-9285-9665c70570f5%2Fanhthe.jpg?table=block&id=8df51c70-aca2-43ca-b61a-857836c692be&spaceId=4aa398d4-2e92-48b7-92f9-4d87362fab14&width=2000&userId=&cache=v2',
+  }
+  
+export  const quang = {
+    name: 'Nguyen Quang Hung',
+    info: 'Student at NEU',
+    position: 'Modeller & Backend Developer',
+    url: 'https://natural-rhubarb-e01.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F4aa398d4-2e92-48b7-92f9-4d87362fab14%2F8cce0b25-15e1-45d4-bce2-68a56c3b9ce5%2FDSC00016.jpg?table=block&id=7aabfac0-e269-41d0-9cf4-aeec8a20d8c6&spaceId=4aa398d4-2e92-48b7-92f9-4d87362fab14&width=2000&userId=&cache=v2',
+  }
+  
+export const hanh = {
+    name: 'Nguyen Thi Hong Hanh',
+    info: 'Student at NEU',
+    position: 'Data Engineering',
+    url: 'https://natural-rhubarb-e01.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F4aa398d4-2e92-48b7-92f9-4d87362fab14%2Ff09cddd2-50df-438e-b4d8-cac8efb0d6df%2FIMG_0869.jpg?table=block&id=cd35c723-d4d3-45f8-a270-e3b6e7d3ff4b&spaceId=4aa398d4-2e92-48b7-92f9-4d87362fab14&width=2000&userId=&cache=v2',
+  }
+
+export  const title4 = {title: 'Who are we?',
+  align: "center"}
+  
+export const text7 ={ title:'',
+  data: ["Our team consists of four talented undergraduate students from National Economic University, sharing the same passion for NLP and machine learning. We are dedicated to pushing the boundaries of what's possible with AI and delivering innovative solutions that meet the needs of our users."],
+  align: "center"};
+  
+export const text8 = {title: "Get in Touch",
+  data:["We're always excited to hear from our users and welcome any feedback or inquiries. Whether you have questions about our platform, suggestions for improvement, or are interested in collaboration opportunities, please don't hesitate to reach out to us. ","Thank you for spending your time on our platform."]
+  ,align: "start"};
+  
