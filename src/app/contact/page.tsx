380232fae4b9a979691b92cf50144fb98a541e19
run time error <quanghung20gg@gmail.com>
2024-02-12 01:00:04 +0700

    ok

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,7 +5,7 @@ import Paragraph from '@/components/Paragraph';
 import {quang,quoc,giang,hanh,title4,text7,text8} from './author';
 
 export default function ContactPage() {
-  // flex flex-col items-stretch grid sm:grid-cols-2 lg:grid-cols-4 space-y-4 sm:space-y-0 sm:space-x-10 gap-4
+  
   return(
     <>
     <Title data={{title: "Authors & Contact", align: "center"}}/>
