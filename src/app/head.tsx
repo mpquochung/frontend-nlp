b900db3c68f25a543f8111eddf165a16be069923
hungmpq <maphquochung@gmail.com>
2025-04-12 17:51:23 +0700

    google verify

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+    return (
+      <>
+        <title>Sentiment & Classification</title>
+        <meta name="google-site-verification" content="wOZRlDNmaaYvkT45rj3ab3wX7FVnSuOID3Y8y3yBwrE" />
+      </>
+    );
+  }
+  
